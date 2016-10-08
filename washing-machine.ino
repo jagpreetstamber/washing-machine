58ccb76a6dfb6e5f43cf364346595bdec9567cd7
Jagpreet Singh Tamber <jagpreetstamber@gmail.com>
2016-10-08 15:32:37 +0530

    Add water drainage and fill up logic

--- washing-machine.ino
+++ washing-machine.ino
@@ -1,6 +1,6 @@
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(12, 11, 10, 9, 8, 7, 6);
+LiquidCrystal lcd(11, 10, 9, 8, 7, 6, 5);
 
 const char washStr[] = "Wash";
 const char modeStr[] = "Mode";
@@ -13,6 +13,8 @@ const char mediumStr[] = "Medium";
 const char highStr[] = "High";
 const char timeStr[] = "Time";
 const char minStr[] = "min";
+const char fillingStr[] = "Filling Up";
+const char drainStr[] = "Draining";
 const char collonStr[] = ":";
 const char spaceStr[] = " ";
 const char plusStr[] = "+";
@@ -34,9 +36,15 @@ const int noOfLcdColumns = 16;
 const int noOfLcdRows = 2;
 
 const int waterInletPin = 13;
-const int selectButtonPin = 19;
-const int upButtonPin = 18;
-const int downButtonPin = 17;
+const int waterDrainPin = 12;
+const int motorControlPin = A0;
+const int selectButtonPin = 2;
+const int upButtonPin = 3;
+const int downButtonPin = 4;
+const int lowSensorPin = A5;
+const int midSensorPin = A4;
+const int highSensorPin = A3;
+const int drainSensorPin = A2;
 
 int stage = ZERO; // 0 - Wash Mode, 1 - Water Level, 2 - Wash Time, 3 - Rinse Time, 4 - Start
 int selectedWashMode = ZERO; // 0 - Wash + Rinse, 1 - Wash, 2 - Rinse
@@ -45,12 +53,22 @@ int selectedWashTime = maxWashTime - 1; // In minutes
 int selectedRinseTime = THREE - 1; // In minutes
 boolean upDownButtonPressed = true;
 boolean selectButtonPressed = true;
+boolean waterLevelReached = false;
 
 void setup() {
   pinMode(selectButtonPin, INPUT);
   pinMode(upButtonPin, INPUT);
   pinMode(downButtonPin, INPUT);
+  pinMode(lowSensorPin, INPUT);
+  pinMode(midSensorPin, INPUT);
+  pinMode(highSensorPin, INPUT);
+  pinMode(drainSensorPin, INPUT);
   pinMode(waterInletPin, OUTPUT);
+  pinMode(waterDrainPin, OUTPUT);
+  pinMode(motorControlPin, OUTPUT);
+  digitalWrite(motorControlPin, LOW);
+  digitalWrite(waterInletPin, LOW);
+  digitalWrite(waterDrainPin, LOW);
   lcd.begin(noOfLcdColumns, noOfLcdRows);
 }
 
@@ -85,14 +103,70 @@ void loop() {
     }
   }
   else if (stage == FOUR) {
-    clearRow(ZERO);
-    lcd.setCursor(ZERO, ZERO);
-    lcd.print(selectedWashMode);
-    lcd.print(selectedWaterLevel);
-    lcd.print(selectedWashTime);
-    lcd.print(selectedRinseTime);
-    delay(300);
+    drainWater();
+    fillUpWater();
+
+    while(true);
   }
+}
+
+void startWashCycle() {
+  clearRow(ZERO);
+  clearRow(ONE);
+  lcd.setCursor(ONE, ZERO);
+  lcd.print(drainStr);
+  lcd.print(spaceStr);
+  lcd.print(waterStr);
+  while (digitalRead(drainSensorPin) == LOW) {
+    digitalWrite(waterDrainPin, HIGH);
+    delay(5000);
+  }
+  digitalWrite(waterDrainPin, LOW);
+  waterLevelReached = false;
+}
+
+void drainWater() {
+  clearRow(ZERO);
+  clearRow(ONE);
+  lcd.setCursor(ONE, ZERO);
+  lcd.print(drainStr);
+  lcd.print(spaceStr);
+  lcd.print(waterStr);
+  while (digitalRead(drainSensorPin) == LOW) {
+    digitalWrite(waterDrainPin, HIGH);
+    delay(5000);
+  }
+  digitalWrite(waterDrainPin, LOW);
+  waterLevelReached = false;
+}
+
+void fillUpWater() {
+  clearRow(ZERO);
+  clearRow(ONE);
+  lcd.setCursor(ZERO, ZERO);
+  lcd.print(fillingStr);
+  lcd.print(spaceStr);
+  lcd.print(waterStr);
+  digitalWrite(waterInletPin, HIGH);
+  while (!waterLevelReached) {
+    int lowSensor = digitalRead(lowSensorPin);
+    int midSensor = digitalRead(midSensorPin);
+    int highSensor = digitalRead(highSensorPin);
+
+    if (selectedWaterLevel == ZERO && lowSensor == LOW) {
+      waterLevelReached = true;
+    }
+    if (selectedWaterLevel == ONE && lowSensor == LOW
+        && midSensor == LOW) {
+      waterLevelReached = true;
+    }
+    if (selectedWaterLevel == TWO && lowSensor == LOW
+        && midSensor == LOW && highSensor == LOW) {
+      waterLevelReached = true;
+    }
+    delay(1000);
+  }
+  digitalWrite(waterInletPin, LOW);
 }
 
 void selectValue(int *value, int numberOfValues) {
